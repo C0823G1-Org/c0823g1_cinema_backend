1d9c0fa6431c423221ebe51c4e1cf7984d75e453
baole1102 <levannhatbao29@gmail.com>
2024-02-28 16:55:35 +0700

    git pull

--- src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
+++ src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
@@ -2,8 +2,20 @@ package com.example.c0823g1_movie_backend.repository;
 
 import com.example.c0823g1_movie_backend.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query(name = "select count(b.account_id),m.name as tenphim\n" +
+            "from booking b\n" +
+            "left join ticket t on b.id = t.booking_id\n" +
+            "left join `schedule` sc on t.schedule_id = sc.id\n" +
+            "left join  movie m on sc.movie_id = m.id\n" +
+            "group by m.name\n" +
+            "limit 8\n" +
+            ";", nativeQuery = true)
+    List<Movie> getAllMovieHot();
 }
