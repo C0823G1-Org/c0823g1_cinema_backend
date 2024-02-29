e0079a660fd8024b3d3e16f7e6fc32ec5e75e7d4
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 15:07:11 +0700

    Update IAccountService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/IAccountService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/IAccountService.java
@@ -5,7 +5,6 @@ import com.example.c0823g1_movie_backend.model.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface IAccountService extends IGeneralService<Account>{
