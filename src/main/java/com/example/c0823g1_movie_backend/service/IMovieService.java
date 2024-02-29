e179686cd41767fc91e96193ca4db5b93405c585
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 15:05:40 +0700

    Update IMovieService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
@@ -1,30 +1,33 @@
 package com.example.c0823g1_movie_backend.service;
 
+import com.example.c0823g1_movie_backend.dto.MovieDTO;
 import com.example.c0823g1_movie_backend.model.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import com.example.c0823g1_movie_backend.dto.MovieDTO;
 import org.springframework.data.repository.query.Param;
+
 import java.util.Date;
 import java.util.List;
 
 
 public interface IMovieService extends IGeneralService {
+    Movie save(Movie movie);
+
+    Movie create(Movie movie);
+
     Page<Movie> getMovieStatistic(Pageable pageable);
 
-    public interface IMovieService extends IGeneralService<Movie> {
-        List<MovieDTO> getAllMovieHot();
+    List<MovieDTO> getAllMovieHot();
 
-        Page<MovieDTO> searchMovie(@Param("name") String value, Pageable pageable);
+    Page<MovieDTO> searchMovie(@Param("name") String value, Pageable pageable);
 
-        List<Movie> getAll();
+    List<Movie> getAll();
 
-        Page<Movie> searchMovieByNameAndPublisher(String name, String publisher, Pageable pageable);
+    Page<Movie> searchMovieByNameAndPublisher(String name, String publisher, Pageable pageable);
 
-        Page<Movie> searchMovieByStartDate(Date startDate, Pageable pageable);
+    Page<Movie> searchMovieByStartDate(Date startDate, Pageable pageable);
 
-        void deleteMovieById(long id);
+    void deleteMovieById(long id);
 
-        void createMovie(Movie movie);
-    }
+    void createMovie(Movie movie);
 }
