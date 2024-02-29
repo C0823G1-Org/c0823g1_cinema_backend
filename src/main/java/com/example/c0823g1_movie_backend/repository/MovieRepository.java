d2c778eb96dbda3e243f03b6f13229e9249f214d
minhthuan196 <minhthuandtu@gmail.com>
2024-02-29 09:27:29 +0700

    ThuanTM commit

--- src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
+++ src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
@@ -1,9 +1,23 @@
 package com.example.c0823g1_movie_backend.repository;
 
 import com.example.c0823g1_movie_backend.model.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query(value = "select id, actor, country, description, director, duration, is_deleted, name,poster, publisher, start_date, ticket_price,trailer from movie where name like :name or publisher like :publisher ;", nativeQuery = true)
+    Page<Movie> searchMovieByNameAndPublisher(@Param("name") String name, @Param("publisher") String publisher, Pageable pageable);
+
+    @Query(value = "select id, actor, country, description, director, duration, is_deleted, name,poster, publisher, start_date, ticket_price,trailer from movie where start_date  =:startDate;", nativeQuery = true)
+    Page<Movie> searchMovieByStartDate(@Param("startDate") Date startDate, Pageable pageable);
+
+    @Query(value = "delete from movie where id :=id;", nativeQuery = true)
+    void deleteMovieById(@Param("id") long id);
 }
--- src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
@@ -1,4 +1,13 @@
 package com.example.c0823g1_movie_backend.service;
 
+import com.example.c0823g1_movie_backend.model.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Date;
+
 public interface IMovieService extends IGeneralService {
+Page<Movie> searchMovieByNameAndPublisher(String name, String publisher, Pageable pageable);
+Page<Movie> searchMovieByStartDate(Date startDate, Pageable pageable);
+void deleteMovieById(long id);
 }
