b59c1e3c5b93a1494592301d2887ef9ab6afaaee
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 09:11:02 +0700

    Update IMovieService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
@@ -1,4 +1,9 @@
 package com.example.c0823g1_movie_backend.service;
 
+import com.example.c0823g1_movie_backend.model.Movie;
+
+import java.util.List;
+
 public interface IMovieService extends IGeneralService {
+    List<Movie> getMovieStatistic();
 }
