0c690823bb3ddac9ad6825afb47212770f2ebf41
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 15:07:06 +0700

    Update MovieDTO.java

--- src/main/java/com/example/c0823g1_movie_backend/dto/MovieDTO.java
+++ src/main/java/com/example/c0823g1_movie_backend/dto/MovieDTO.java
@@ -1,11 +1,5 @@
 package com.example.c0823g1_movie_backend.dto;
 
-import com.example.c0823g1_movie_backend.model.Schedule;
-import com.example.c0823g1_movie_backend.model.Ticket;
-
-import java.time.LocalDate;
-import java.util.Set;
-
 public interface MovieDTO {
     Long getAccountId();
     String getName();
