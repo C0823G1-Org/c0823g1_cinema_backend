5a9b30d603d32ad46a443c5a585dfb60976b1bf5
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 09:11:05 +0700

    Update MovieService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/MovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/MovieService.java
@@ -1,9 +1,16 @@
 package com.example.c0823g1_movie_backend.service;
 
+import com.example.c0823g1_movie_backend.model.Movie;
+import com.example.c0823g1_movie_backend.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService implements IMovieService{
+    @Autowired
+    private MovieRepository movieRepository;
     @Override
     public Object save(Object o) {
         return null;
@@ -11,6 +18,12 @@ public class MovieService implements IMovieService{
 
     @Override
     public Object create(Object o) {
+        return null;
+    }
+
+
+    @Override
+    public List<Movie> getMovieStatistic() {
         return null;
     }
 }
