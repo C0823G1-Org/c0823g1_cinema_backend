b627049a0966a59ac7f8e0fe3a3757811618e973
Levando10 <102398741+Levando10@users.noreply.github.com>
2024-02-29 08:34:58 +0700

    up booking do

--- src/main/java/com/example/c0823g1_movie_backend/controller/BookingRestController.java
+++ src/main/java/com/example/c0823g1_movie_backend/controller/BookingRestController.java
@@ -1,5 +1,7 @@
 package com.example.c0823g1_movie_backend.controller;
 
+import com.example.c0823g1_movie_backend.service.IBookingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("/booking")
 public class BookingRestController {
+    @Autowired
+    private IBookingService iBookingService;
+
+
 }
