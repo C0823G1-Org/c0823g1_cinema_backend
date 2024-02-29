e7185ff87183cbb51a0c31038c21e5ae45ef329a
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 09:42:08 +0700

    Update MovieRepository.java

--- src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
+++ src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
@@ -2,8 +2,20 @@ package com.example.c0823g1_movie_backend.repository;
 
 import com.example.c0823g1_movie_backend.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query(value = "SELECT m.name AS movie_name, COUNT(b.id) AS ticket_count " +
+            "FROM movie m " +
+            "JOIN schedule s ON m.id = s.movie_id " +
+            "JOIN booking b ON s.id = b.schedule_id " +
+            "WHERE s.is_deleted = 0 AND m.is_deleted = 0 AND b.is_deleted = 0 " +
+            "GROUP BY m.name " +
+            "ORDER BY ticket_count DESC " +
+            "LIMIT 20", nativeQuery = true)
+    List<Movie> findTop20MoviesByRevenue();
 }
