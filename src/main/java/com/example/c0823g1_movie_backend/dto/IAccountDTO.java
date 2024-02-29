d49bd72483fb79e19d52ca61b079dc513cc0be3a
TAT08122k4 <140997922+TAT08122k4@users.noreply.github.com>
2024-02-29 08:45:07 +0700

    tuanDemo

--- src/main/java/com/example/c0823g1_movie_backend/dto/IAccountDTO.java
+++ src/main/java/com/example/c0823g1_movie_backend/dto/IAccountDTO.java
@@ -1,9 +1,13 @@
 package com.example.c0823g1_movie_backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public interface IAccountDTO {
     Integer getId();
+
     String getAccountName();
     String getAddress();
     LocalDate getBirthday();
