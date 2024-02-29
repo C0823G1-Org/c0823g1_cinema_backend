3ede8766f27ef9619f319516d86fab4aacb47084
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 15:07:08 +0700

    Update MovieRepository.java

--- src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
+++ src/main/java/com/example/c0823g1_movie_backend/repository/MovieRepository.java
@@ -12,9 +12,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-
-import java.util.List;
-
 import java.util.List;
 
 @Repository
