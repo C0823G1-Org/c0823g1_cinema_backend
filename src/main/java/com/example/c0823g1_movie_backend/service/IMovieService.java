eb0622e76fe2236f8767ffae539bee29aea25dce
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 13:55:36 +0700

    Update IMovieService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/IMovieService.java
@@ -1,9 +1,11 @@
 package com.example.c0823g1_movie_backend.service;
 
 import com.example.c0823g1_movie_backend.model.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IMovieService extends IGeneralService {
-    List<Movie> getMovieStatistic();
+    Page<Movie> getMovieStatistic(Pageable pageable);
 }
