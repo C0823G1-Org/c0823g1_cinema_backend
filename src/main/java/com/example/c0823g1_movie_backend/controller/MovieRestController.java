1b32095a5ff7c14c33dce34cfc380decf57223c3
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 09:10:57 +0700

    Update MovieRestController.java

--- src/main/java/com/example/c0823g1_movie_backend/controller/MovieRestController.java
+++ src/main/java/com/example/c0823g1_movie_backend/controller/MovieRestController.java
@@ -1,7 +1,28 @@
 package com.example.c0823g1_movie_backend.controller;
 
+import com.example.c0823g1_movie_backend.dto.MovieStatisticDTO;
+import com.example.c0823g1_movie_backend.model.Movie;
+import com.example.c0823g1_movie_backend.service.IMovieService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/movie")
 public class MovieRestController {
+    @Autowired
+    private IMovieService movieService;
+
+    @GetMapping("/statistics")
+    private ResponseEntity<MovieStatisticDTO> movieStatistics() {
+//        List<MovieStatisticDTO> statisticDTOList = movieService.
+        return  null;
+    }
 }
