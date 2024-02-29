3a22712b6db959efeb608f7ea16aed4f9bd61012
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-02-29 15:05:43 +0700

    Update MovieService.java

--- src/main/java/com/example/c0823g1_movie_backend/service/MovieService.java
+++ src/main/java/com/example/c0823g1_movie_backend/service/MovieService.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.util.Date;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,58 +16,62 @@ public class MovieService implements IMovieService {
     @Autowired
     private MovieRepository movieRepository;
 
-    @Service
-    public class MovieService implements IMovieService {
-        @Autowired
-        private MovieRepository movieRepository;
+    @Override
+    public List<MovieDTO> getAllMovieHot() {
+        return movieRepository.getAllMovieHot();
+    }
 
-        @Override
-        public List<MovieDTO> getAllMovieHot() {
-            return movieRepository.getAllMovieHot();
-        }
+    @Override
+    public Page<MovieDTO> searchMovie(String value, Pageable pageable) {
+        return movieRepository.searchMovie("%" + value + "%", pageable);
+    }
 
-        @Override
-        public Page<MovieDTO> searchMovie(String value, Pageable pageable) {
-            return movieRepository.searchMovie("%" + value + "%", pageable);
-        }
+    @Override
+    public List<Movie> getAll() {
+        return movieRepository.findAll();
+    }
 
-        @Override
-        public List<Movie> getAll() {
-            return movieRepository.findAll();
-        }
+    @Override
+    public Movie save(Movie movie) {
+        return null;
+    }
 
-        @Override
-        public Movie save(Movie movie) {
-            return null;
-        }
+    @Override
+    public Movie create(Movie movie) {
+        return null;
+    }
 
-        @Override
-        public Movie create(Movie movie) {
-            return null;
-        }
+    @Override
+    public Page<Movie> getMovieStatistic(Pageable pageable) {
+        return movieRepository.findTop20MoviesByRevenue(pageable);
+    }
 
-        @Override
-        public Page<Movie> getMovieStatistic(Pageable pageable) {
-            return movieRepository.findTop20MoviesByRevenue(pageable);
-        }
+    public void createMovie(Movie movie) {
+        movieRepository.createMovie(movie);
+    }
 
-        public void createMovie(Movie movie) {
-            movieRepository.createMovie(movie);
-        }
+    @Override
+    public Page<Movie> searchMovieByNameAndPublisher(String name, String publisher, Pageable pageable) {
+        return movieRepository.searchMovieByNameAndPublisher("%" + name + "%", "%" + publisher + "%", pageable);
+    }
 
-        @Override
-        public Page<Movie> searchMovieByNameAndPublisher(String name, String publisher, Pageable pageable) {
-            return movieRepository.searchMovieByNameAndPublisher("%" + name + "%", "%" + publisher + "%", pageable);
-        }
+    @Override
+    public Page<Movie> searchMovieByStartDate(Date startDate, Pageable pageable) {
+        return movieRepository.searchMovieByStartDate(startDate, pageable);
+    }
 
-        @Override
-        public Page<Movie> searchMovieByStartDate(Date startDate, Pageable pageable) {
-            return movieRepository.searchMovieByStartDate(startDate, pageable);
-        }
+    @Override
+    public void deleteMovieById(long id) {
 
-        @Override
-        public void deleteMovieById(long id) {
+    }
 
-        }
+    @Override
+    public Object save(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object create(Object o) {
+        return null;
     }
 }
