ff937af52144a8eee8c537300f53dea9e48b978c
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-03-01 14:59:52 +0700

    Update MovieStatisticDTO.java

--- src/main/java/com/example/c0823g1_movie_backend/dto/MovieStatisticDTO.java
+++ src/main/java/com/example/c0823g1_movie_backend/dto/MovieStatisticDTO.java
@@ -2,5 +2,6 @@ package com.example.c0823g1_movie_backend.dto;
 
 public interface MovieStatisticDTO {
     String getMovie_name();
-    Integer getTicket_count();
+    Integer getSold_ticket();
+    Double getRevenue();
 }
