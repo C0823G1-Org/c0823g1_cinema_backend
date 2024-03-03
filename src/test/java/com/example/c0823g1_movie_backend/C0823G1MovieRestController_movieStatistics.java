ce441ae84120bf6e5445f6fcb9599ab5c99e018c
duydd-c0623g1 <daoducduy.work@gmail.com>
2024-03-03 18:15:08 +0700

    Update C0823G1MovieRestController_movieStatistics.java

--- src/test/java/com/example/c0823g1_movie_backend/C0823G1MovieRestController_movieStatistics.java
+++ src/test/java/com/example/c0823g1_movie_backend/C0823G1MovieRestController_movieStatistics.java
@@ -14,12 +14,21 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class C0823G1MovieRestController_movieStatistics {
     @Autowired
     private MockMvc mockMvc;
-
+    /**
+     * This function is used to test the return value of records from database
+     * @author DuyDD
+     * @Time 6:13pm 03/03/24
+     */
     @Test
     public void movieStatistics_5() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/movie/statistics"))
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError());
     }
+    /**
+     * This function is used to test the return value of records from database
+     * @author DuyDD
+     * @Time 6:13pm 03/03/24
+     */
     @Test
     public void movieStatistics_nonEmptyList() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/movie/statistics"))
